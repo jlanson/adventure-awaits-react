1ee5049a61101aa78fcdcd22a1b02e62a935b54c
Jonathan Lanson <jlanson@uwaterloo.ca>
2022-05-25 18:37:49 -0400

    Updated navbar and button, setup adventuresection

---
+++ src/components/AdventureSection.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./AdventureContainer.css";
+import { Button } from "./Button";
+import "../App.css"
+
+function AdventureSection() {
+    return(
+        <div className='hero-container'>
+            <h1>ADVENTURE AWAITS</h1>
+            <p>Join in now</p>
+                <div className = "hero-btns">
+                    <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
+                        CLICK HERE!
+                    </Button>
+                    <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
+                        CLICK HERE!
+                    </Button>
+                </div>
+        </div>
+    )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,31 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
+import "./Navbar.css"
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () =>{
         setClick(!click); 
     }
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    }
+
     const closeMobileMenu = () => setClick(false);
+
+    useEffect(()=>{
+        showButton();
+    },[]);
+
+    window.addEventListener('resize', showButton);
   return (
     <>
         <nav className='navbar'>
@@ -20,26 +38,27 @@ function Navbar() {
                 </div>
                 <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
                     <li className='nav-item'>
-                        <Link to='/' className='nav-links' onCLick={closeMobileMenu}>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                             Home
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/services' className='nav-links' onCLick={closeMobileMenu}>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
                             Services
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/products' className='nav-links' onCLick={closeMobileMenu}>
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
                             Products
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/sign-up' className='nav-links-mobile' onCLick={closeMobileMenu}>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
                             Sign Up
                         </Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </div>
         </nav>
     </>
